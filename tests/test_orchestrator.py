d87cbbfd21baf5919d1af6c5ce7278970a1361e2
Byron Himes <byro93@live.com>
2024-05-08 08:45:01 +0200

    Refactor config attr access
    Co-authored-by: Christoph Zwerschke <cito@online.de>

--- tests/test_orchestrator.py
+++ tests/test_orchestrator.py
@@ -98,9 +98,7 @@ async def test_access_request(
     """
     test_user = await joint_fixture.user_dao.get_by_id(TEST_USER.id)
 
-    event_type_to_use = joint_fixture.config.model_dump()[
-        f"access_request_{event_type}_event_type"
-    ]
+    event_type_to_use = getattr(joint_fixture.config, f"access_request_{event_type}_event_type")
 
     assert event_type_to_use
 
