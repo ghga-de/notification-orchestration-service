3226dae75dfb6bc14f96c715227f5ce39f13e709
TheByronHimes <TheByronHimes@gmail.com>
2025-07-17 08:32:06 +0000

    Use construct method on dao factory

--- src/nos/inject.py
+++ src/nos/inject.py
@@ -43,11 +43,12 @@ async def prepare_core(
     config: Config,
 ) -> AsyncGenerator[OrchestratorPort, None]:
     """Constructs and initializes all core components and their outbound dependencies."""
-    dao_factory = MongoDbDaoFactory(config=config)
-    user_dao = await get_user_dao(dao_factory=dao_factory)
-    access_request_dao = await get_access_request_dao(dao_factory=dao_factory)
-
-    async with KafkaEventPublisher.construct(config=config) as event_publisher:
+    async with (
+        MongoDbDaoFactory.construct(config=config) as dao_factory,
+        KafkaEventPublisher.construct(config=config) as event_publisher,
+    ):
+        user_dao = await get_user_dao(dao_factory=dao_factory)
+        access_request_dao = await get_access_request_dao(dao_factory=dao_factory)
         notification_emitter = NotificationEmitter(
             config=config, event_publisher=event_publisher
         )
